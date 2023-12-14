64bff18dffd136cc7106db025fd8e7450b88609a
Tim Douglas <mail@timdouglas.co.uk>
2023-12-14 20:59:15 +0000

    add Product type and working getProducts query

--- src/logic/slices/products.ts
+++ src/logic/slices/products.ts
@@ -2,14 +2,33 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const API_BASE = 'http://localhost:3010';
 
+export type Product = {
+  id: number;
+  slug: string;
+  title: string;
+  vendor: string;
+  tags: string[];
+  published: boolean;
+  url: string;
+  image_src: string;
+  option_value: string;
+  sku: string;
+  price: number;
+  subscription: boolean;
+  subscription_discount: number | '';
+};
+
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${API_BASE}/products/`,
+    baseUrl: `${API_BASE}/`,
   }),
   endpoints: (builder) => ({
-    getProducts: builder.query<any, void>({
-      query: () => '',
+    getProducts: builder.query<Product[], void>({
+      query: () => 'products',
+      // ensure we only see published products
+      transformResponse: (resp: Product[]) =>
+        resp.filter((product) => product.published),
     }),
   }),
 });
